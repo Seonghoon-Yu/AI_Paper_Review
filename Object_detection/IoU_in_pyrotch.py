3939a481a3886ba31457e6897bc12c6656fc7a59
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-01-02 17:52:26 +0900

    Create IoU_in_pyrotch.py

---
+++ Object_detection/IoU_in_pyrotch.py
@@ -0,0 +1,42 @@
+import torch
+
+def intersection_over_union(boxes_preds, boxes_labels, box_format='midpoint'):
+
+    # box 좌표는 midpoint, corners 두 가지 형식이 있습니다.
+
+    # boxes_preds shape : (N,4) N은 bboxes의 개수 입니다.
+    # boxes_labels shape : (N,4)
+
+    if box_format == 'midpoint':
+    box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
+    box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
+    box1_x2 = boxes_preds[..., 0:1] + boxes_preds[..., 2:3] / 2
+    box1_y2 = boxes_preds[..., 1:2] + boxes_preds[..., 3:4] / 2
+    box2_x1 = boxes_labels[..., 0:1] - boxes_labels[..., 2:3] / 2
+    box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
+    box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
+    box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
+
+    if box_format == 'corners':
+        box1_x1 = boxes_preds[..., 0:1] # (N,1)
+        box1_y1 = boxes_preds[..., 1:2]
+        box1_x2 = boxes_preds[..., 2:3]
+        box1_y2 = boxes_preds[..., 3:4]
+        box2_x1 = boxes_labels[..., 0:1]
+        box2_y1 = boxes_labels[..., 1:2]
+        box2_x2 = boxes_labels[..., 2:3]
+        box2_y2 = boxes_labels[..., 3:4]
+
+    x1 = torch.max(box1_x1, box2_x1)
+    y1 = torch.max(box1_y1, box2_y1)
+    x2 = torch.max(box1_x2, box2_x2)
+    y2 = torch.max(box1_y2, box2_y2)
+
+    # .clamp(0)은 intersect하지 않는 경우입니다.
+    # 최소값을 0으로 설정합니다.
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-6)
