353f890d544f3a708c05614ba771601c7f88429f
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-01-11 17:42:58 +0900

    Update darknet.py

--- Object_detection/YOLO_v3_from_scratch/detector/darknet.py
+++ Object_detection/YOLO_v3_from_scratch/detector/darknet.py
@@ -39,3 +39,119 @@ def parse_cfg(cfgfile):
     blocks.append(block)
 
     return blocks
+
+# nn.Module class를 사용하여 layers에 대한 module인 create_modules 함수를 정의합니다.
+# 입력은 parse_cfg 함수에서 반환된 blocks를 취합니다.
+def create_modules(blocks):
+    net_info = blocks[0] # 입력과 전처리에 대한 정보를 저장합니다.
+    module_list = nn.ModuleList()
+    prev_filters = 3
+    output_filters = []
+    
+    for index, x in enumerate(blocks[1:]):
+        module = nn.Sequential()
+        
+        # block의 type을 확인합니다.
+        # block에 대한 새로운 module을 생성합니다.
+        # module_list에 append 합니다.
+        
+        if (x['type'] == 'convolutional'):
+            # layer에 대한 정보를 얻습니다.
+            activation = x['activation']
+            try:
+                batch_normalize = int(x['batch_normalize'])
+                bias = False
+            except:
+                batch_normalize = 0
+                bias = True
+
+            filters = int(x['filters'])
+            padding = int(x['pad'])
+            kernel_size = int(x['size'])
+            stride = int(x['stride'])
+
+            if padding:
+                pad = (kernel_size - 1) // 2
+            else:
+                pad = 0
+
+            # convolutional layer를 추가합니다.
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias = bias)
+            module.add_module('conv_{0}'.format(index),conv)
+
+            # Batch Norm Layer를 추가합니다.
+            if batch_normalize:
+                bn = nn.BatchNorm2d(filters)
+                module.add_module('batch_norm_{0}'.format(index),bn)
+
+            # activation을 확인합니다.
+            # YOLO에서 Leaky ReLU 또는 Linear 입니다.
+            if activation == 'leaky':
+                activn = nn.LeakyReLU(0.1, inplace = True)
+                module.add_module('leaky_{0}'.format(index), activn)
+
+        # upsampling layer 입니다.
+        # Bilinear2dUpsampling을 사용합니다.
+        elif (x['type'] == 'upsample'):
+            stride = int(x['stride'])
+            upsample = nn.Upsample(scale_factor = 2, mode = 'bilinear')
+            module.add_module('upsample_{}'.format(index), upsample)
+
+        # route layer 입니다.
+        elif (x['type'] == 'route'):
+            x['layers'] = x['layers'].split(',')
+            # route 시작
+            start = int(x['layers'][0])
+            # 1개만 존재하면 종료
+            try:
+                end = int(x['layers'][1])
+            except:
+                end = 0
+            # 양수인 경우
+            if start > 0:
+                start = start - index
+            if end > 0:
+                end = end - index
+            route = EmptyLayer()
+            module.add_module('route_{0}'.format(index), route)
+            # route layer에서 출력되는 filter의 수를 저장하는 filter 변수를 갱신합니다.
+            if end < 0:
+                filters = output_filters[index + start] + output_filters[index + end]
+            else:
+                filters = output_filters[index + start]
+
+        # skip connection에 해당하는 shortcut
+        elif x['type'] == 'shortcut':
+            shortcut = EmptyLayer()
+            module.add_module('shortcut_{}'.format(index), shortcut)
+
+        # YOLO는 detection layer입니다.
+        elif x['type'] == 'yolo':
+            mask = x['mask'].split(',')
+            mask = [int(x) for x in mask]
+
+            anchors = x['anchors'].split(',')
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors),2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module('Detection_{}'.format(index), detection)
+
+        # bookkeeping
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+
+    return (net_info, module_list)
+
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+
+class DetectionLayer(nn.Module):
+    def __init__(self,anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
