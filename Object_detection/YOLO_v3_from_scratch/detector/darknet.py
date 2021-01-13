e94045b390a5154f40af07c6f9f3f3273e6cfeaa
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-01-13 19:02:54 +0900

    Update darknet.py

--- Object_detection/YOLO_v3_from_scratch/detector/darknet.py
+++ Object_detection/YOLO_v3_from_scratch/detector/darknet.py
@@ -1,4 +1,5 @@
 # 필요한 library import
+from util import *
 from __future__ import division
 
 import torch
@@ -6,6 +7,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
+
+### Test
+# model = Darknet("cfg/yolov3.cfg")
+# inp = get_test_input()
+# pred = model(inp, torch.cuda.is_available())
+# print (pred)
+###
+
+def get_test_input():
+    img = cv2.imread('dog-cycle-car.pnp')
+    img = cv2.resize(img, (416,416))        # 입력 크기로 resize 합니다.
+    img_ = img[:,:,::-1].transpose((2,0,1)) # BGR - > RGB, HxWxC -> CxHxW
+    img_ = img_[np.newaxis,:,:,:]/255.0     # (배치를 위한) 0 채널 추가하고 normalize 합니다.
+    img_ = torch.from_numpy(img_).float()   # 정수로 변환합니다.
+    img_ = Variable(img_)                   # 변수로 변환합니다.
+
 
 # parse_cfg 함수 정의하기, 구성 파일의 경로를 입력으로 받습니다.
 def parse_cfg(cfgfile):
@@ -162,38 +179,74 @@ class Darknet(nn.Module):
         self.blocks = parse_cfg(cfgfile)
         self.net_if, self.module_list - create_module(self, blocks)
         
-def forward(self, x, CUDA):
-    modules = self.blocks[1:]
-    outputs = {} # route layer에 대한 출력값을 저장합니다.
-    
-    write = 0 # 이것은 추후에 설명하겠습니다.
-    for i, module in enumerate(modules):
-        module_type = (module['type'])
-        
-        if module_type == 'convolutional' or module_type == 'upsample':
-            x = self.module_list[i](x)
-        
-        elif module_type == 'route':
-            layers = module['layers']
-            layers = [int(a) for a in layers]
-            
-            if (layers[0]) > 0:
-                layers[0] = layers[0] - i
-            
-            if len(layers) == 1:
-                x = outputs[i + (layers[0])]
-            
-            else:
-                if (layers[1]) > 0:
-                    layers[1] = layers[1] - i
-                    
-                map1 = outputs[i + layers[0]]
-                map2 = outputs[i + layers[1]]
-                
-                x = torch.cat((map1, map2), 1)
-            
+    def forward(self, x, CUDA):
+        modules = self.blocks[1:]
+        outputs = {} # route layer에 대한 출력값을 저장합니다.
+
+        write = 0 # 이것은 추후에 설명하겠습니다.
+        for i, module in enumerate(modules):
+            module_type = (module['type'])
+
+            if module_type == 'convolutional' or module_type == 'upsample':
+                x = self.module_list[i](x)
+
+            elif module_type == 'route':
+                layers = module['layers']
+                layers = [int(a) for a in layers]
+
+                if (layers[0]) > 0:
+                    layers[0] = layers[0] - i
+
+                if len(layers) == 1:
+                    x = outputs[i + (layers[0])]
+
+                else:
+                    if (layers[1]) > 0:
+                        layers[1] = layers[1] - i
+
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+
+                    x = torch.cat((map1, map2), 1)
+
             elif module_type == 'shortcut':
                 from_ = int(module['from'])
                 x = outputs[i-1] + outputs[i+from_]
-                
+
+            elif module_type == 'yolo':
+
+                anchors = self.module_list[i][0].anchors
+                # 입력 차원을 얻습니다.
+                inp_dim = int(self.net_info['height'])
+
+                # 클래스의 수를 얻습니다.
+                num_classes = int(module['classes'])
+
+                # util.py에 있는 predict_transform 함수를 이용하여 변환
+                x = x.data
+                x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
+                if not write:    # collector가 초기화 되지 않은 경우
+                    detections = x
+                    write = 1
+
+                else:
+                    detections = torch.cat((detections, x), 1)
+
+            outputs[i] = x
+
+        return detections
+    
+    def load_weights(self, weightfile):
+        # weights file 열기
+        fp = open(weightfile, 'rb')
+        
+        # 첫 5개 값은 header 정보
+        # 1. Magor version number
+        # 2. Minor version number
+        # 3. Subversion number
+        # 4, 5. (training 동안) 신경망에 의하여 학습된 이미지
+        header = np.fromfile(fp, dtype = np.int32, count = 5)
+        self.header = torch.from_numpy(header)
+        self.seen = self.header[3]
+        
         
