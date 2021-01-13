749d0993a1e68b32d604b7bcf2508d3c5ff35d1b
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-01-13 15:58:01 +0900

    Update darknet.py

--- Object_detection/YOLO_v3_from_scratch/detector/darknet.py
+++ Object_detection/YOLO_v3_from_scratch/detector/darknet.py
@@ -155,3 +155,45 @@ class DetectionLayer(nn.Module):
     def __init__(self,anchors):
         super(DetectionLayer, self).__init__()
         self.anchors = anchors
+        
+class Darknet(nn.Module):
+    def __init__(self, cfgfile):
+        supper(Darknet, self).__init__()
+        self.blocks = parse_cfg(cfgfile)
+        self.net_if, self.module_list - create_module(self, blocks)
+        
+def forward(self, x, CUDA):
+    modules = self.blocks[1:]
+    outputs = {} # route layer에 대한 출력값을 저장합니다.
+    
+    write = 0 # 이것은 추후에 설명하겠습니다.
+    for i, module in enumerate(modules):
+        module_type = (module['type'])
+        
+        if module_type == 'convolutional' or module_type == 'upsample':
+            x = self.module_list[i](x)
+        
+        elif module_type == 'route':
+            layers = module['layers']
+            layers = [int(a) for a in layers]
+            
+            if (layers[0]) > 0:
+                layers[0] = layers[0] - i
+            
+            if len(layers) == 1:
+                x = outputs[i + (layers[0])]
+            
+            else:
+                if (layers[1]) > 0:
+                    layers[1] = layers[1] - i
+                    
+                map1 = outputs[i + layers[0]]
+                map2 = outputs[i + layers[1]]
+                
+                x = torch.cat((map1, map2), 1)
+            
+            elif module_type == 'shortcut':
+                from_ = int(module['from'])
+                x = outputs[i-1] + outputs[i+from_]
+                
+        
