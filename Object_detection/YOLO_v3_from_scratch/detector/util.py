8f58bfa2286d7945d4182c045ca4fcd1e6b800c3
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-01-13 17:07:02 +0900

    Update util.py

--- Object_detection/YOLO_v3_from_scratch/detector/util.py
+++ Object_detection/YOLO_v3_from_scratch/detector/util.py
@@ -1,1 +1,59 @@
-aa
+from __future__ import division
+
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F 
+from torch.autograd import Variable
+import numpy as np
+import cv2 
+
+def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
+    
+    batch_size = prediction.size(0)
+    stride = inp_dim // prediction.size(2)
+    grid_size = inp_dim // stride
+    bbox_attrs = 5 + num_classes
+    num_anchors = len(anchors)
+    
+    prediction = prediction.view(batch_size, bbox_attrs * num_anchors, grid_size * grid_size)
+    prediction = prediction.transpose(1,2).contiguous()
+    prediction = prediction.view(batch_size, grid_size * grid_size * num_anchors, bbox_attrs)
+    
+    anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
+    
+    # 중심 x,y 좌표와 object confidence를 SIgmoid 합니다.
+    prediction[:,:,0] = torch.sigmoid(prediction[:,:,0])
+    prediction[:,:,1] = torch.sigmoid(prediction[:,:,1])
+    prediction[:,:,4] = torch.sigmoid(prediction[:,:,4])
+    
+    # 중심 offset을 추가합니다.
+    grid = np.arange(grid_size)
+    a, b = np.meshgrid(grid, grid)
+    
+    x_offset = torch.FloatTensor(a).view(-1, 1)
+    y_offset = torch.FloatTensor(b).view(-1, 1)
+    
+    if CUDA:
+        x_offset = x_offset.cuda()
+        y_offset = y_offset.cuda()
+        
+    x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1,num_anchors).view(-1,2).unsqueeze(0)
+    
+    prediction[:,:,:2] += x_y_offset
+    
+    # 높이와 넓이를 log space 변환합니다.
+    anchors = torch.FloatTensor(anchors)
+    
+    if CUDA:
+        anchors = anchors.cuda()
+        
+    anchors = anchors.repeat(grid_size * grid_size, 1).unsqueeze(0)
+    prediction[:,:,2:4] = torch.exp(prediction[:,:,2:4]) * anchors
+                                    
+    # class score에 sigmoid activation을 적용합니다.
+    prediction[:,:,5: 5 + num_classes] = torch.sigmoid((prediction[:,:,5 : 5 + num_classes]))
+    
+    # detection map을 입력 이미지의 크기로 resize 합니다.
+    prediction[:,:,:4] *= stride
+    
+    return prediction
