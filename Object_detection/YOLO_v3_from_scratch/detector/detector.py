625d286b6c60c3b56eeefeb38835eaa917dfb1b3
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-01-30 23:39:13 +0900

    Update detector.py

--- Object_detection/YOLO_v3_from_scratch/detector/detector.py
+++ Object_detection/YOLO_v3_from_scratch/detector/detector.py
@@ -54,5 +54,145 @@ CUDA = torch.cuda.is_available()
 num_classes = 80    #For COCO
 classes = load_classes("data/coco.names")
 
+# 신경망 설정하기
+print("Loading network.....")
+model = Darknet(args.cfgfile)
+model.load_weights(args.weightsfile)
+print("Network successfully loaded")
 
+model.net_info["height"] = args.reso
+inp_dim = int(model.net_info["height"])
+assert inp_dim % 32 == 0 
+assert inp_dim > 32
+
+# GPU가 사용가능하면, GPU에서 모델 구동하기
+if CUDA:
+    model.cuda()
+
+# model을 evaluation mode로 설정하기
+model.eval()
+
+
+read_dir = time.time()
+# Detection 분석
+try:
+    imlist = [osp.join(osp.realpath('.'), images, img) for img in os.listdir(images)]
+except NotADirectoryError:
+    imlist = []
+    imlist.append(osp.join(osp.realpath('.'), images))
+except FileNotFoundError:
+    print ("No file or directory with the name {}".format(images))
+    exit()
+    
+if not os.path.exists(args.det):
+    os.makedirs(args.det)
+
+load_batch = time.time()
+loaded_ims = [cv2.imread(x) for x in imlist]
+
+
+def letterbox_image(img, inp_dim):
+    ''' padding을 사용하여 aspect ratio가 변화하지 않고 이미지를 resize 합니다.'''
+    img_w, img_h = img.shape[1], img.shape[0]
+    w, h = inp_dim
+    new_w = int(img_w * min(w/img_w, h/img_h))
+    new_h = int(img_h * min(w/img_w, h/img_h))
+    resized_image = cv2.resize(img, (new_w,new_h), interpolation = cv2.INTER_CUBIC)
+    
+    canvas = np.full((inp_dim[1], inp_dim[0], 3), 128)
+
+    canvas[(h-new_h)//2:(h-new_h)//2 + new_h,(w-new_w)//2:(w-new_w)//2 + new_w,  :] = resized_image
+    
+    return canvas
+
+
+def prep_image(img, inp_dim):
+    """
+    신경망에 입력하기 위한 이미지 준비
+    변수를 반환합니다. 
+    """
+
+    img = cv2.resize(img, (inp_dim, inp_dim))
+    img = img[:,:,::-1].transpose((2,0,1)).copy()
+    img = torch.from_numpy(img).float().div(255.0).unsqueeze(0)
+    return img
+
+# 이미지를 위한 PyTorch Variables
+im_batches = list(map(prep_image, loaded_ims, [inp_dim for x in range(len(imlist))]))
+
+# 기존 이미지의 차원을 담고 있는 List
+im_dim_list = [(x.shape[1], x.shape[0]) for x in loaded_ims]
+im_dim_list = torch.FloatTensor(im_dim_list).repeat(1,2)
+
+if CUDA:
+    im_dim_list = im_dim_list.cuda()
+
+# 배치 생성하기
+leftover = 0
+if (len(im_dim_list) % batch_size):
+   leftover = 1
+
+if batch_size != 1:
+   num_batches = len(imlist) // batch_size + leftover            
+   im_batches = [torch.cat((im_batches[i*batch_size : min((i +  1)*batch_size,
+                       len(im_batches))]))  for i in range(num_batches)]  
+    
+write = 0
+start_det_loop = time.time()
+for i, batch in enumerate(im_batches):
+    # 이미지 불러오기
+    start = time.time()
+    if CUDA:
+        batch = batch.cuda()
+
+    prediction = model(Variable(batch, volatile = True), CUDA)
+
+    prediction = write_results(prediction, confidence, num_classes, nms_conf = nms_thesh)
+
+    end = time.time()
+
+    if type(prediction) == int:
+
+        for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
+            im_id = i*batch_size + im_num
+            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+            print("{0:20s} {1:s}".format("Objects Detected:", ""))
+            print("----------------------------------------------------------")
+        continue
+
+    prediction[:,0] += i*batch_size    # 배치에있는 index 속성을 imlist에서 index 속성으로 변환합니다. 
+
+    if not write:                      # 출력값이 초기화되지 않은 경우
+        output = prediction  
+        write = 1
+    else:
+        output = torch.cat((output,prediction))
+
+    for im_num, image in enumerate(imlist[i*batch_size: min((i +  1)*batch_size, len(imlist))]):
+        im_id = i*batch_size + im_num
+        objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
+        print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+        print("{0:20s} {1:s}".format("Objects Detected:", " ".join(objs)))
+        print("----------------------------------------------------------")
+
+    if CUDA:
+        torch.cuda.synchronize()     
+
+try:
+    output
+except NameError:
+    print ("No detections were made")
+    exit()
+        
+# 패딩된 이미지에 대한 영역의 경계를 고려하여 바운딩박스의 좌표 변환
+im_dim_list = torch.index_select(im_dim_list, 0, output[:,0].long())
+
+scaling_factor = torch.min(inp_dim/im_dim_list,1)[0].view(-1,1)
+
+
+output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim_list[:,0].view(-1,1))/2
+output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim_list[:,1].view(-1,1))/2
+
+# 기존 이미지에 대한 바운딩 박스의 좌표를 얻기 위해 rescaling 되돌리기 
+output[:,1:5] /= scaling_factor
 
